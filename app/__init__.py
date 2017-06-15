0f82d1a373eacf0246d0d1b6baf97756cc33b1a1
JordanRahal <jordan.rahal@gmail.com>
2017-06-15 17:57:25 -0500

    added and modified files to create login screen

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,6 @@
-###this is our python initialization file. Its the first thing that gets read
-##from here, it will call on Flask to read in the rest of the app we have
-
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object('config') ##enables Flask to read and use oour 'config.py file'
+app.config.from_object('config')
 
 from app import views
--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,7 @@
-###forms.py
-##purpose is to create a login form that users will use to identify with the system
-##login is by use of OpenID
-
 from flask_wtf import Form
 from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired #DataRequired validator simply checks that the field is not submitted empty
+from wtforms.validators import DataRequired
 
 class LoginForm(Form):
-	openid = StringField('openid', validators=[DataRequired()])
-	remember_me = BooleanField('remember_me', default=False)
+    openid = StringField('openid', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
