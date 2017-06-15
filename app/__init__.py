5b1e001a84f35083576dc2c633bd9d381fbf34dc
JordanRahal <jordan.rahal@gmail.com>
2017-06-15 14:00:51 -0500

    added config to init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,9 @@
+###this is our python initializiation file. Its the first thing that gets read
+##from here, it will call on Flask to read in the rest of the app we have
+
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object('config') ##enables Flask to read and use oour 'config.py file'
+
 from app import views
